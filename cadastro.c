4bf39dbb472833669f034c7484c4dfbfac93c1c0
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-22 13:47:19 -0300

    funcao de pesquisa

--- cadastro.c
+++ cadastro.c
@@ -8,10 +8,14 @@ int cpf[SIZE];
 int op;
 
 void cadastro();
+void pesquisa();
 
 
 int main(int argc, char *argv[]) {
 	cadastro();
+	pesquisa();
+	
+	
 }
 
 void cadastro(){
@@ -27,12 +31,53 @@ void cadastro(){
 		printf("\n Digite o cpf: ");
 		scanf("%d", &cpf[linha]);
 		
-		printf("/n Digite 1 para continuar ou outro valor para sair !");
+		printf("\n Digite 1 para continuar ou outro valor para sair !");
 		scanf("%d", &op);
 		linha++;
 		
 	}while(op == 1);
 }
 
-
+void pesquisa(){
+	
+	int cpfPesquisa;
+	int i;
+	char nomePesquisa[50];
+	do{
+		printf("\nDigite 1 para pesquisar por cpf ou 2 para pesquisar por nome: ");
+		scanf("%d", &op);
+		switch(op){
+			case 1:
+				printf("\n Digite o cpf: ");
+				scanf("%d", &cpfPesquisa);
+				for( i=0 ;i<SIZE; i++){
+					if(cpf[i]==cpfPesquisa){
+						printf("\nName: %s \nEmail: %s \nCPF: %d", nome[i], email[i], cpf[i]);
+					}
+				}
+				
+				break;
+			case 2:
+					printf("\n Digite o nome: ");
+				scanf("%s", nomePesquisa);
+				for( i=0 ;i<SIZE; i++){
+					if(strcmp(nome[i], nomePesquisa) == 0){
+						printf("\nName: %s \nEmail: %s \nCPF: %d", nome[i], email[i], cpf[i]);
+					}
+				}
+				break;
+			default:
+			
+				printf("Opção invalida!");
+			
+				break;	
+		}
+		
+		printf("\n Digite 1 para continuar!");
+		scanf("%d", &op);
+	}while(op == 1);
 }
+
+	
+	
+	
